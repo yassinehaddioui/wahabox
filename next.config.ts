a3abe09b6cd8ec0ed2fc71b66d887ce1915a8e8a
Yassine Haddioui WSL <yassine@haddioui.com>
2026-06-26 17:43:48 +0100

    WIP

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   output: process.env.NODE_ENV === 'production' ? 'standalone' : undefined,
   serverExternalPackages: ['@prisma/client', '@prisma/adapter-pg'],
-  eslint: { ignoreDuringBuilds: true },
 }
 
 export default nextConfig
